b64d86f7b35ca95a66319a4ddaae1c6023be6123
nicolasklz <nicolaskleinhentz@gmail.com>
2019-11-15 11:49:44 +0100

    + fabrique des bateaux par époque et par taille

---
+++ src/main/java/model/fabriqueEpoque/FabriqueEpoque.java
@@ -0,0 +1,15 @@
+package main.java.model.fabriqueEpoque;
+
+public interface FabriqueEpoque {
+
+    /*Bateau creerBateau1 ();
+    Bateau creerBateau2 ();
+    Bateau creerBateau2V2 ();
+    Bateau creerBateau3 ();
+    Bateau creerBateau3V2 ();
+    Bateau creerBateau3V3 ();
+    Bateau creerBateau4 ();
+    Bateau creerBateau4V2 ();
+    Bateau creerBateau4V3 ();*/
+
+}
---
+++ src/main/java/model/fabriqueEpoque/fabriqueEpoque1/FabriqueEpoque1.java
@@ -0,0 +1,44 @@
+package main.java.model.fabriqueEpoque.fabriqueEpoque1;
+
+import main.java.model.fabriqueEpoque.FabriqueEpoque;
+
+public class FabriqueEpoque1 implements FabriqueEpoque {
+
+    /*public Bateau creerBateau1 () {
+        return new Bateau(1, 5, Arme.LEGERE);
+    }
+
+    public Bateau creerBateau2 () {
+        return new Bateau(2, 10, [Arme.LEGERE, Arme.LEGERE])
+    }
+
+    public Bateau creerBateau2V2 () {
+        return new Bateau(2, 10, [Arme.MOYENNE, null])
+    }
+
+    public Bateau creerBateau3 () {
+        return new Bateau(3, 15, [Arme.LEGERE, Arme.MOYENNE, Arme.LEGERE])
+    }
+
+    public Bateau creerBateau3V2 () {
+        return new Bateau(3, 15, [Arme.MOYENNE, Arme.MOYENNE, null])
+    }
+
+    public Bateau creerBateau3V3 () {
+        return new Bateau(3, 15, [null, Arme.LOURDE, null])
+    }
+
+    public Bateau creerBateau4 () {
+        return new Bateau(4, 20, [Arme.ATOMIQUE, null, null, null])
+    }
+
+    public Bateau creerBateau4V2 () {
+        return new Bateau(4, 20, [Arme.LOURDE, Arme.MOYENNE, null, null])
+    }
+
+    public Bateau creerBateau4V3 () {
+        return new Bateau(4, 20, [Arme.LEGERE, Arme.MOYENNE, Arme.MOYENNE, Arme.LEGERE])
+    }
+    */
+
+}
